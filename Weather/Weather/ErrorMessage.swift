fecb5c134d8fbe742a7cb3be640ddd75276d8165
mal0070 <mal0070@naver.com>
2022-08-11 03:16:13 +0900

    error alert

---
+++ Weather/Weather/ErrorMessage.swift
@@ -0,0 +1,11 @@
+//
+//  ErrorMessage.swift
+//  Weather
+//
+//  Created by 이민아 on 2022/08/11.
+//
+
+import Foundation
+struct ErrorMessage: Codable {
+    let message: String
+}
--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -40,22 +40,42 @@ class ViewController: UIViewController {
         self.maxTempLabel.text = "최고: \(Int(weatherInformation.temp.minTemp - 273.15)) ℃"
     }
     
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "에러", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+        self.present(alert, animated: true)
+    }
+    
     func getCurrentWeather(cityName: String){
         guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=3bb7f416a0be629ff6bf6abae119e4c1") else { return }
         
         let session = URLSession(configuration: .default)
         session.dataTask(with: url) { [weak self] data, response, error in //weak self: 순환참조 해결
+            
+            let successRange = (200..<300) //에러 잡기!
+            
             guard let data = data, error == nil else { return }
             let decoder = JSONDecoder() //json 객체에서 데이터로!
-            guard let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data) else {return} //try? -> decoding에 실패하면 error
-            //debugPrint(weatherInformation)
             
-            //네트워크 작업은 별도의 스레드에서 진행된다. 따라서 메인스레드에서 작업가능하도록
-            DispatchQueue.main.async {
-                self?.weatherStackView.isHidden = false
-                self?.configureView(weatherInformation: weatherInformation)
+            if let response = response as? HTTPURLResponse, successRange.contains(response.statusCode){ //범위내 -> 통신성공
+                guard let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data) else {return} //try? -> decoding에 실패하면 error
+                //debugPrint(weatherInformation)
+                
+                //네트워크 작업은 별도의 스레드에서 진행된다. 따라서 메인스레드에서 작업가능하도록
+                DispatchQueue.main.async {
+                    self?.weatherStackView.isHidden = false
+                    self?.configureView(weatherInformation: weatherInformation)
+                }
+            } else{
+                guard let errorMessage = try? decoder.decode(ErrorMessage.self, from: data) else {return}
+                //debugPrint(errorMessage)
+                //메인스레드에서 alert이 실행되어야함
+                DispatchQueue.main.async {
+                    self?.showAlert(message: errorMessage.message)
+                }
             }
-            }.resume() //작업실행
+            
+        }.resume() //작업실행
     }
 
 }
